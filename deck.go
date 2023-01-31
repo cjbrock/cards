33bc8c6826cd59eea33b9959ac8e800d36f6ae58
Corinna Brock Moore <corinna.brock@gmail.com>
2023-01-31 15:00:40 -0500

    multi-nested loops and return values and _

--- deck.go
+++ deck.go
@@ -4,6 +4,19 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+	return cards
+}
+
 func (d deck) printCards() {
 	for i, card := range d {
 		fmt.Println(i, card)
